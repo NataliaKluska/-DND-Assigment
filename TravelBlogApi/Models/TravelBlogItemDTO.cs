c90d6038e6a72291a785df15e913291fe6afee85
Irina-Maria Mereuta <92296855+arttuulips@users.noreply.github.com>
2024-10-15 00:29:02 +0300

    Add files via upload

---
+++ TravelBlogApi/Models/TravelBlogItemDTO.cs
@@ -0,0 +1,8 @@
+namespace TravelBlogApi.Models;
+
+public class TravelBlogItemDTO
+{
+    public long Id { get; set; }
+    public string? Name { get; set; }
+    public bool IsComplete { get; set; }
+}
