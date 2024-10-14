863203495c04a435066e8c23c7ebcdfed37d7b54
Irina-Maria Mereuta <92296855+arttuulips@users.noreply.github.com>
2024-10-15 00:26:31 +0300

    Delete Models directory

--- Models/TravelBlogItemDTO.cs
+++
@@ -1,8 +0,0 @@
-namespace TravelBlogApi.Models;
-
-public class TravelBlogItemDTO
-{
-    public long Id { get; set; }
-    public string? Name { get; set; }
-    public bool IsComplete { get; set; }
-}
